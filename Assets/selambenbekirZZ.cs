4916bf14cc5ec6bc1ff4e8bdb017c0eee80723fc
FatihKaraatli <78076948+FatihKaraatli@users.noreply.github.com>
2022-02-20 02:05:27 +0300

    Add files via upload

---
+++ Assets/selambenbekirZZ.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class selambenbekirZZ : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Debug.Log("ss");
+    }
+}
---
+++ Assets/sifirlama.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sifirlama : MonoBehaviour
+{
+
+    public void Sıfırla()
+    {
+        PlayerPrefs.DeleteKey("skor");
+        PlayerPrefs.DeleteAll();
+    }
+
+    public void Hile()
+    {
+        PlayerPrefs.SetFloat("ticket", (int)(PlayerPrefs.GetFloat("ticket") + 500000));
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("AnaGiris");
+    }
+
+    
+}
